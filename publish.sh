b437cd8d65b2dbd03212d4986edea33d4db5b273
lmm <myliming@gmail.com>
2023-10-12 10:48:17 +0800

    测试 jenkins

--- publish.sh
+++ publish.sh
@@ -3,7 +3,8 @@
 #gradle build
 
 #docker build -t nocoolming/site-service:$VERSION .
-imageName="nocoolming/site-service:${date +%Y%m%d%H%M%S%N}"
+version=$(date +%Y%m%d%H%M%S%N)
+imageName="nocoolming/site-service:${version}"
 
 echo "image name: $imageName"
 #docker build -t nocoolming/site-service:$(date +%Y%m%d%H%M%S%N) .
