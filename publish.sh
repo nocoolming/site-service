442076b5f7c584c43f6916baa986c3cb7dd056fd
lmm <myliming@gmail.com>
2023-10-12 22:50:04 +0800

    测试 jenkins

--- publish.sh
+++ publish.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+
+echo "ssh -p 8822 -tt -o StrictHostKeyChecking=no root@sunmoon.zone ls"
+ssh -p 8822 -tt -o StrictHostKeyChecking=no root@sunmoon.zone ls
+
 #docker build -t nocoolming/site-service:$VERSION .
 version=$(date +%Y%m%d%H%M%S%N)
 imageName="nocoolming/site-service:${version}"
