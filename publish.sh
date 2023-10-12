7622a4f6bfe2701396e469b6902cc74e2ff4ca09
lmm <myliming@gmail.com>
2023-10-12 23:06:47 +0800

    删除了无用的注释代码 完成了ci/cd

--- publish.sh
+++ publish.sh
@@ -1,19 +1,12 @@
 #!/usr/bin/env bash
 
-
-echo "ssh -p 8822 -tt -o StrictHostKeyChecking=no root@sunmoon.zone ls"
-ssh -p 8822 -tt -o StrictHostKeyChecking=no root@sunmoon.zone ls
-
-#docker build -t nocoolming/site-service:$VERSION .
 version=$(date +%Y%m%d%H%M%S%N)
 imageName="nocoolming/site-service:${version}"
 
 echo "image name: $imageName"
 
-#ls
 ./gradlew build
 
-#ls build/libs/
 echo "docker build -t $imageName ."
 docker build -t $imageName .
 
