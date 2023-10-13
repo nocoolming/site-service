cf607d8ca7280fd0923f3147c7ad18a209e99b36
lmm <myliming@gmail.com>
2023-10-13 22:54:07 +0800

    增加参数指定 application.yml

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 mv /app/*.jar /app/app.jar
 export JAVA_OPTS="-XX:+UnlockExperimentalVMOptions -XX:+UseZGC -Xms256m -Xmx256m -Xss1m"
 
-java  --spring.config.location=/app/application.yml \
+#java  --spring.config.location=/app/application.yml \
+java \
  ${JAVA_OPTS} -jar /app/app.jar
 
