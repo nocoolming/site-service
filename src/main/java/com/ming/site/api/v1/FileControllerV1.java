41e697ed25a8300c9f82cee84163489897447ffb
lmm <myliming@gmail.com>
2023-10-22 10:31:09 +0800

    没有 配上传文件的大小。

--- src/main/java/com/ming/site/api/v1/FileControllerV1.java
+++ src/main/java/com/ming/site/api/v1/FileControllerV1.java
@@ -9,14 +9,12 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
+@CrossOrigin
 @RestController
 @RequestMapping("site/v1/file")
 public class FileControllerV1
