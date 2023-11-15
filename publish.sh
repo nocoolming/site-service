38f51f69c2487e1a4380a756e753e61707fc51b8
lmm <myliming@gmail.com>
2023-11-15 20:43:35 +0800

    修改了服务器地址

--- publish.sh
+++ publish.sh
@@ -14,8 +14,8 @@ docker build -t $imageName .
 echo "docker push $imageName"
 docker push $imageName
 
-echo "ssh -p xxx -tt -o StrictHostKeyChecking=no xxx@174.137.56.192 sh $version"
-ssh -p 924 -tt -o StrictHostKeyChecking=no lmm@ras0.com /home/lmm/projects/api/publish.sh $version
+echo "ssh -p xxx -tt -o StrictHostKeyChecking=no xxx@xxxx sh $version"
+ssh -p 924 -tt -o StrictHostKeyChecking=no lmm@174.137.56.192/ /home/lmm/projects/api/publish.sh $version
 
 #echo "ssh -p 8822 -tt -o StrictHostKeyChecking=no root@sunmoon.zone /root/projects/api/publish.sh $version"
 #ssh -p 8822 -tt -o StrictHostKeyChecking=no root@sunmoon.zone /root/projects/api/publish.sh $version
