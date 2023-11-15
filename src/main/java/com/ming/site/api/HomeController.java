dc0fcd8403122b204435e54ad6bf0b8822e03d5d
lmm <myliming@gmail.com>
2023-11-15 21:07:27 +0800

    i测试环境。

--- src/main/java/com/ming/site/api/HomeController.java
+++ src/main/java/com/ming/site/api/HomeController.java
@@ -3,28 +3,38 @@ package com.ming.site.api;
 import com.ming.site.common.Result;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.sql.DataSource;
 
 @RestController
 public class HomeController {
     private static final Logger log = LoggerFactory.getLogger(HomeController.class);
 
+    @Autowired
+    DataSource dataSource;
+
     @GetMapping("/")
-    String home(){
+    String home() {
         return "Site service";
     }
 
 
     @GetMapping("site")
-    Result<String> site(){
+    Result<String> site() {
         return Result.ok("Site service");
     }
 
     @GetMapping(value = "/robots.txt", produces = MediaType.TEXT_PLAIN_VALUE)
     public String getRobotsTxt() {
-        return "User-agent: *\n" +
-                "Disallow: /\n";
+        return "User-agent: *\n" + "Disallow: /\n";
+    }
+
+    @GetMapping("site/dataSource")
+    Result<DataSource> dataSource() {
+        return Result.ok(dataSource);
     }
 }
