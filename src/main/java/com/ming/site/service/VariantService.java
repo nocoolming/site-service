3f1e87032a0c93bb639078b4406b95403aec3b4d
lmm <myliming@gmail.com>
2023-12-22 09:58:22 +0800

    实现了getVariantsByProductId接口。

--- src/main/java/com/ming/site/service/VariantService.java
+++ src/main/java/com/ming/site/service/VariantService.java
@@ -2,6 +2,10 @@ package com.ming.site.service;
 
 import com.ming.site.model.Variant;
 
+import java.util.List;
+
 public interface VariantService
         extends CrudService<Variant, Long> {
+
+    List<Variant> getVariantsByProductId(long productId);
 }
--- src/main/java/com/ming/site/service/impl/VariantServiceImpl.java
+++ src/main/java/com/ming/site/service/impl/VariantServiceImpl.java
@@ -4,11 +4,30 @@ import com.ming.site.mapper.VariantMapper;
 import com.ming.site.model.Variant;
 import com.ming.site.service.AbstractService;
 import com.ming.site.service.VariantService;
+import com.mybatisflex.core.query.QueryWrapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class VariantServiceImpl
         extends AbstractService<Variant, Long, VariantMapper>
         implements VariantService {
+    private static final Logger log = LoggerFactory.getLogger(VariantServiceImpl.class);
 
+    @Autowired
+    VariantMapper variantMapper;
+
+    @Override
+    public List<Variant> getVariantsByProductId(long productId) {
+        List<Variant> list = variantMapper.selectListByQuery(
+                QueryWrapper.create().where(
+                        "product_id", productId
+                )
+        );
+        return list;
+    }
 }
