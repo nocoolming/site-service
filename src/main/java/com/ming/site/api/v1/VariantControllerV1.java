293bc6cdac295683e0ff3f62e734c03b691f4c38
lmm <myliming@gmail.com>
2024-01-15 20:56:41 +0800

    新增一个接口返回value map variant

--- src/main/java/com/ming/site/api/v1/VariantControllerV1.java
+++ src/main/java/com/ming/site/api/v1/VariantControllerV1.java
@@ -2,6 +2,7 @@ package com.ming.site.api.v1;
 
 import com.ming.site.api.AbstractControllerV1;
 import com.ming.site.common.Result;
+import com.ming.site.model.Value;
 import com.ming.site.model.Variant;
 import com.ming.site.service.NotAllOptionsHaveValuesException;
 import com.ming.site.service.VariantService;
@@ -10,6 +11,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @RestController
 @RequestMapping("site/v1/variant")
@@ -25,10 +28,28 @@ public class VariantControllerV1
     }
 
     @GetMapping("productId/{productId}")
-    Result<List<Variant>> getVariantsByProductId(@PathVariable long productId){
+    Result<List<Variant>> getVariantsByProductId(@PathVariable long productId) {
         List<Variant> list = service.getVariantsByProductId(productId);
 
         return Result.ok(list);
     }
 
+    @GetMapping("getValuesMapVariants/productId/{productId}")
+    Result<Map<Map<String, Value>, Variant>> getValuesMapVariants(@PathVariable long productId) {
+        List<Variant> list = service.getVariantsByProductId(productId);
+
+        Map<Map<String, Value>, Variant> result = new ConcurrentHashMap<>();
+
+        for (Variant variant : list) {
+            Map<String, Value> map = new ConcurrentHashMap<>();
+
+            for (Value value : variant.getValues()) {
+                map.put(value.getTitle(), value);
+            }
+
+            result.put(map, variant);
+        }
+
+        return Result.ok(result);
+    }
 }
