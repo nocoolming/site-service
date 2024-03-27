7e4eb0c55e8ab8e99925ec3871e44e5d960ff1ba
lmm <myliming@gmail.com>
2024-03-27 12:15:11 +0800

    修改了依赖。

--- src/main/java/com/ming/site/Application.java
+++ src/main/java/com/ming/site/Application.java
@@ -10,11 +10,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.SecurityFilterChain;
 
 @SpringBootApplication
 @MapperScan("com.ming.site.mapper")
@@ -25,21 +20,6 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
-	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.csrf(AbstractHttpConfigurer::disable)
-				.authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
-						authorizationManagerRequestMatcherRegistry.requestMatchers(HttpMethod.DELETE).hasRole("ADMIN")
-								.requestMatchers("/admin/**").hasAnyRole("ADMIN")
-								.requestMatchers("/user/**").hasAnyRole("USER", "ADMIN")
-								.requestMatchers("/login/**").permitAll()
-								.anyRequest().authenticated())
-				.httpBasic(Customizer.withDefaults())
-				.sessionManagement(httpSecuritySessionManagementConfigurer ->
-						httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-
-		return http.build();
-	}
 	@Bean
 	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
 		return args -> {
